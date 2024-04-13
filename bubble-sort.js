e59ad0c935602f63ba1b1a059bec76aefd0c79cf
tabryan90 <tabryan1990@gmail.com>
2024-04-13 13:04:49 -0400

    complete

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,18 +1,25 @@
 
 function bubbleSort(arr) {
+  let swapped = true;
 
+  while (swapped) {
+    swapped = false;
     // Iterate through the array
-
+    for ( let i = 0; i < arr.length; i++ ) {
       // If the current value is greater than its neighbor to the right
+      if ( arr[i] > arr[i + 1]) {
         // Swap those values
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
 
         // Do not move this console.log
         console.log(arr.join(","));
 
+        swapped = true;
+      }
+    }
     // If you get to the end of the array and no swaps have occurred, return
-
     // Otherwise, repeat from the beginning
-
+  }
 }
 
 module.exports = bubbleSort;
---
+++ scratch.js
@@ -0,0 +1,12 @@
+function bubbleSort(arr) {
+
+    // Iterate through the array
+
+      // If the current value is greater than its neighbor to the right
+        // Swap those values
+
+      // If you get to the end of the array and no swaps have occurred, return
+
+      // Otherwise, repeat from the beginning
+
+  }
